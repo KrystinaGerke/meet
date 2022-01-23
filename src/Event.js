8501b2c440578def24363270f7e5dd42365fbab2
KrystinaGerke <83786005+KrystinaGerke@users.noreply.github.com>
2022-01-22 20:24:55 -0500

    Event first run

--- src/Event.js
+++ src/Event.js
@@ -1,9 +1,40 @@
-import React, { Component } from "react";
-import { mockData } from './mock-data';
+import React, { Component } from 'react';
 
-class Event extends Component {
-  render() {
-    return <div></div>;
-  }
+export class Event extends Component {
+
+    state = {
+        collapsed: true
+    };
+
+    handleClick= ()  => {
+        this.setState({
+            collapsed: !this.state.collapsed
+        });
+    };
+
+    render(){
+        const { event } = this.props;
+        const { collapsed } = this.state;
+       
+        
+    return <div className='event'>
+        
+        <h1 className='summary'>{event.summary}</h1>
+        <p className='dateTime'>{event.dateTime}</p>
+        <p className='timeZone'>{event.timeZone}</p>
+        <p className='location'>@{event.summary} | {event.location}</p>
+        if(collapsed === true){
+            <button className='show-details'
+        onClick={this.handleClick}>show Details</button>
+        }
+        else{
+            <div className="details-view">
+            <link className='htmlLink'>{event.htmlLink}</link>
+            <p className='description'>{event.description}</p>
+            <button className='hide-details'
+        onClick={this.handleClick}>hide Details</button></div>
+        }
+    </div>;  
+    }
+    
 }
-export default Event;
