1551042fe083d7c146019f8ad72f2d26242207da
KrystinaGerke <83786005+KrystinaGerke@users.noreply.github.com>
2022-01-28 14:07:40 -0500

    4.9- added event chart

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import { EventList } from './EventList';
 import { CitySearch } from './CitySearch';
 import { NumberOfEvents } from './NumberOfEvents';
 import { extractLocations, getEvents } from './api';
+import { ErrorAlert, WarningAlert } from './Alert';
+import { ScatterChart, Scatter, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
 
 
 
@@ -67,11 +69,24 @@ export class App extends Component {
     } 
   };
 
+  getData = () => {
+    const {locations, events} = this.state;
+    const data = locations.map((location)=>{
+      const number = events.filter((event) => event.location === location).length
+      const city = location.split(', ').shift()
+      return {city, number};
+    })
+    return data;
+  };
   
 
   render () {
+    const { locations, numberOfEvents } = this.state;
     return (
     <div className="App">
+      <h1>Meet App</h1>
+      <h4>Choose your nearest city</h4>
+    { !navigator.onLine ? (<WarningAlert text='You are in offline mode!' />) : (<WarningAlert text=' ' />)}
       <CitySearch 
       locations={this.state.locations} 
       updateEvents={this.updateEvents}/>
@@ -79,6 +94,21 @@ export class App extends Component {
       numberOfEvents={this.state.numberOfEvents}
       updateNumberOfEvents={this.updateNumberOfEvents}
       errorText ={this.state.errorText}/>
+      <h4>Events in each city</h4>
+
+      <ResponsiveContainer height={400} >
+      <ScatterChart
+          margin={{
+            top: 20, right: 20, bottom: 20, left: 20,
+          }}
+        >
+          <CartesianGrid />
+          <XAxis type="category" dataKey="city" name="city" />
+          <YAxis type="number" dataKey="number" name="number of events" />
+          <Tooltip cursor={{ strokeDasharray: '3 3' }} />
+          <Scatter data={this.getData()} fill="#8884d8" />
+        </ScatterChart>
+        </ResponsiveContainer>
       <EventList events={this.state.events}/>
       
     </div>
